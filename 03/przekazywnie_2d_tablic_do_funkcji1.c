7abaf85a905ff4ce7f68728e40b69c353a1f6363
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-04-10 15:44:08 +0200

    Create przekazywnie_2d_tablic_do_funkcji1.c

---
+++ 03/przekazywnie_2d_tablic_do_funkcji1.c
@@ -0,0 +1,14 @@
+//Przekazywanie tab[][] do funkcji: 
+//pseudicode:
+
+void func(float (*x)[12], int a, int b);
+
+int main(){
+    float a[10][12];
+
+    return 0;
+}
+
+void func(float (*x)[12], int a, int b){
+
+}
