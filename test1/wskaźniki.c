b18579f070ec03226120591e8be3181308c16ea6
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-03 17:38:39 +0200

    Create wskaźniki.c

---
+++ test1/wskaźniki.c
@@ -0,0 +1,18 @@
+/*
+x = 1.5;
+&x = adres 0;
+
+p_x = adres 0;
+*p_x = 1,5;
+&p_x = adres 1;
+
+pp_x = adres 1;
+*pp_x = adres 0;
+**pp_x = 1.5;
+&pp_x = adres 2;
+
+ppp_x = adres 2;
+*ppp_x = adres 1;
+**ppp_x = adres 0;
+***ppp_x = 1.5;
+*/
