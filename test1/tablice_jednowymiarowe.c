d05c508ecdb283de2e520a44d1e7559f596bce25
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-04 11:26:43 +0200

    Update tablice_jednowymiarowe.c

--- test1/tablice_jednowymiarowe.c
+++ test1/tablice_jednowymiarowe.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SIZE 6
 
-#define ROWS 3 // Trzy wiersze
-#define COLS 2 // Dwie kolumny
-
-void d_array(double *start, double *end, double value){
+void set_array(double *start, double *end, double value){
     while(start<end){
-        *start++= value;
+        *start++ = value;
     }
 }
 
-void d_array2(double *start, size_t n, double value){
+void set_array_2(double *start, size_t n, double value){
     for(size_t i = 0; i < n; i++){
-        *(start +i) = value;
+        *(start+i) = value;
     }
 }
 
 int main() {
-    int n = 4;
-    double *array = malloc(n * sizeof(double));
+    double array1[SIZE];
+    size_t n = SIZE;
+    double array2[n];
+    double *array3 = malloc(n*sizeof(double));
+
     //1
-    for(size_t i = 0; i < n; i++){
-        printf("%f, ", *(array+i));
+    set_array(array1, array1 + SIZE, 12.);
+    for (size_t i = 0; i <SIZE; i++){
+        printf("%f ", *(array1+i));
     }
     printf("\n");
 
-    d_array(array, array+n, 13.);
-
-    for(size_t i = 0; i < n; i++){
-        printf("%f, ", *(array+i));
+    set_array(array2, array2 + SIZE, 13.);
+    for (size_t i = 0; i <SIZE; i++){
+        printf("%f ", *(array2+i));
     }
     printf("\n");
 
+    set_array(array3, array3 + SIZE, -100.);
+    for (size_t i = 0; i <SIZE; i++){
+        printf("%f ", *(array3+i));
+    }
+    printf("\n\n\n");
 
     //2
-    d_array2 (array, n, 23.);
-    for(size_t i = 0; i < n; i++){
-        printf("%f, ", *(array+i));
+    set_array_2(array1, SIZE, 10293.);
+    for (size_t i = 0; i <SIZE; i++){
+        printf("%f ", *(array1+i));
     }
     printf("\n");
-      
-    //Zwolnienie pamieci!
-    free(array);
-    array = NULL:
-    return 0;
+
+    free(array3);
+    array3 = NULL;
 }
-// 0.000000, 0.000000, 0.000000, 0.000000, 
-// 13.000000, 13.000000, 13.000000, 13.000000, 
-// 23.000000, 23.000000, 23.000000, 23.000000, 
+// 12.000000 12.000000 12.000000 12.000000 12.000000 12.000000 
+// 13.000000 13.000000 13.000000 13.000000 13.000000 13.000000 
+// -100.000000 -100.000000 -100.000000 -100.000000 -100.000000 -100.000000 
+
+
+// 10293.000000 10293.000000 10293.000000 10293.000000 10293.000000 10293.000000 
