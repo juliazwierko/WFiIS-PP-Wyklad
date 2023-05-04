030aba8aec628102dec71b7f0b77cac2bcad0134
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-04 11:39:11 +0200

    Update tablice2D_z_automatycznym_przydziałem_pamięci.c

--- test1/tablice2D_z_automatycznym_przydziałem_pamięci.c
+++ test1/tablice2D_z_automatycznym_przydziałem_pamięci.c
@@ -1,57 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define ROWS 3 // Trzy wiersze
 #define COLS 2 // Dwie kolumny
 
-void array_2d(double (*row)[COLS], size_t rows, double value){
-    for(size_t w = 0; w < rows; ++w){
-        for(size_t k = 0; k < COLS; ++k)
-            *(*(row + w) + k) = value;
+void set_2d_array(double (*row)[COLS], size_t rows, double value){
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t w = 0; w < COLS; ++w){
+            *(*(row+i) + w) = value;
+        }
     }
 }
 
-void arra_2d_2wersja(size_t cols, double (*row)[cols], size_t rows, double value){
-    for(size_t w = 0; w < rows; ++w){
-        for(size_t k = 0; k < cols; ++k){
-            *(*(row+w)+k) = value;
+void set_2d_array_2(size_t cols, double (*row)[cols], size_t rows, double value){
+    for(size_t i = 0; i < rows; ++i){
+        for(size_t w = 0; w < cols; ++w){
+            *(*(row + i) + w) = value;
         }
     }
 }
 
 int main() {
     double tab1[ROWS][COLS];
+    double tab2[ROWS][COLS];
 
-    // 1 funkcja 
-    array_2d(tab1, ROWS, 12.);
-    for(int i = 0; i < ROWS; i++){
-        for(int j = 0; j < COLS; j++){
-            printf("%f ", *(*(tab1+i)+j));
+    set_2d_array(tab1, ROWS, 11.99);
+    
+
+    for(size_t w = 0; w < ROWS; w++){
+        for(size_t i = 0; i < COLS; i++){
+            printf("%f, ", *(*(tab1+w)+i));
         }
         printf("\n");
     }
     printf("\n");
 
-    // 2 funkcja
-    arra_2d_2wersja(COLS, tab1, ROWS, 1120.);
-    for(int i = 0; i < ROWS; i++){
-        for(int j = 0; j < COLS; j++){
-            printf("%f ", *(*(tab1+i)+j));
+    set_2d_array_2(COLS, tab2, ROWS, 12.5);
+    for(size_t w = 0; w < ROWS; w++){
+        for(size_t i = 0; i < COLS; i++){
+            printf("%f, ", *(*(tab2+w)+i));
         }
         printf("\n");
     }
-
-    // tablica o stalych rozmiarach zdefinowanych prez zmienne
-    size_t rows = ROWS + 1;
-    size_t cols = COLS +1;
-    int array2[rows][cols];
+    printf("\n");
     return 0;
 }
-// 12.000000 12.000000 
-// 12.000000 12.000000 
-// 12.000000 12.000000 
+11.990000, 11.990000, 
+11.990000, 11.990000, 
+11.990000, 11.990000, 
 
-// 1120.000000 1120.000000 
-// 1120.000000 1120.000000 
-// 1120.000000 1120.000000 
+12.500000, 12.500000, 
+12.500000, 12.500000, 
+12.500000, 12.500000,
