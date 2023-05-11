a174af4d64fdba5c320c68a01391c98519e6222d
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-11 15:00:22 +0200

    Update struktury_7_tablice_struktur.c

--- 07/struktury_7_tablice_struktur.c
+++ 07/struktury_7_tablice_struktur.c
@@ -3,10 +3,17 @@
 #include <string.h>
 #include <stddef.h>
 
+//1
 struct item{
     int id; 
     char name[40]; 
     int os;
+};
+
+//2
+struct critter{
+    const char *name;
+    const char *species;
 };
 
 void wypisanie(struct item t){
@@ -16,12 +23,15 @@ void wypisanie(struct item t){
 }
 
 int main (int argc, char **argv){
+    //1
     struct item inventory[100];
     inventory[7].id = 883; 
-    inventory[20].name[0] = '\0';   
-
+    inventory[20].name[0] = '\0';  
     for(int i = 0; i < 100; i++)
         wypisanie(inventory[i]);
-
+    
+    struct critter muppets[] = { {"Kermit", "frog" }, {"Piggy", "pig"} };
+    //wewnetrzne nawiasy klamrowe sa opcjonalne
+    
     return 0;
 }
