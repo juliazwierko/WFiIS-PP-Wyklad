aa1536a12d9e5a1a5331962cda26e95e4820b66c
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-15 14:39:52 +0200

    Create 03_#include.c

---
+++ 11/03_#include.c
@@ -0,0 +1,8 @@
+Dyrektywa #include służy do dołączenia zawartości pliku do kodu źródłowego, 
+np. plików nagłówkowych.
+  
+Ogólna postać dyrektywy:#include nazwapliku
+Przykład:
+#include <stdio.h>
+#include "../moje_funkcje.h"
+
