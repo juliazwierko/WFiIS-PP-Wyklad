80ca63602921e8872c09e47c30030ee7509fd8f2
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-15 12:39:56 +0200

    Create 02_standartowe_strumienie

---
+++ 09/02_standartowe_strumienie.c
@@ -0,0 +1,9 @@
+*W języku C strumień oznacza dowolne źródło wejścia lub wyjścia.
+*W języku C zdefiniowano standardowe strumienie:
+    *stdin - standardowe wejście - klawiatura.
+    *stdout - standardowe wyjście - ekran
+    *stderr - standardowe wyjście błędów - ekran
+*Przekierowanie wejścia ./a.out < input.dat
+*Przekierowanie wyjścia ./a.out > output.dat
+*Przekierowanie wyjścia i wyjścia ./a.out >out.dat <in.dat
+*Przekierowanie wejścia błędów ./a.out 2> err.dat
