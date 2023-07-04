f3e94164c6ca042c0dc3ed6c3f04b831eec68f95
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-07-04 12:53:46 +0200

    Create 8(dodać_poprawki!!!).c

---
+++ samodzielne_cwiczenia/8.c
@@ -0,0 +1,55 @@
+/*
+  Zadanie
+  Prosze napisac fragmenty programu, ktory pozwola zainicjowac i 
+  wypisac nastepujace zmienne 
+  
+  klasa.numer;
+  (klasa.nauczyciel+4)->nazwisko;
+  klasa.uczen[1].nazwisko;
+  klasa.uczen[3].imie[2]
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct uczen_ {
+    char imie[20];
+    char nazwisko[20];
+};
+
+struct nauczyciel_ {
+    char imie[20];
+    char nazwisko[20];
+};
+
+struct klasa {
+    int numer;
+    struct uczen_ uczen[25];
+    struct nauczyciel_ *nauczyciel;
+};
+
+int main(int argc, char *argv[]) {
+    struct klasa mojaKlasa; // Deklaracja zmiennej klasa
+    mojaKlasa.numer = 4;
+    strcpy((mojaKlasa.nauczyciel + 4)->nazwisko, "Szybko");
+    strcpy(mojaKlasa.uczen[1].nazwisko, "Zviarko");
+    mojaKlasa.uczen[3].imie[2] = 'J';
+
+    printf("mojaKlasa.numer: %d\n", mojaKlasa.numer);
+    printf("(mojaKlasa.nauczyciel + 4)->nazwisko: %s\n", (mojaKlasa.nauczyciel + 4)->nazwisko);
+    printf("mojaKlasa.uczen[1].nazwisko: %s\n", mojaKlasa.uczen[1].nazwisko);
+    printf("mojaKlasa.uczen[3].imie[2]: %c\n", mojaKlasa.uczen[3].imie[2]);
+
+    scanf("%d", &mojaKlasa.numer);
+    scanf("%s", (mojaKlasa.nauczyciel + 4)->nazwisko);
+    scanf("%s", mojaKlasa.uczen[1].nazwisko);
+    scanf(" %c", &(mojaKlasa.uczen[3].imie[2]));
+
+    printf("mojaKlasa.numer: %d\n", mojaKlasa.numer);
+    printf("(mojaKlasa.nauczyciel + 4)->nazwisko: %s\n", (mojaKlasa.nauczyciel + 4)->nazwisko);
+    printf("mojaKlasa.uczen[1].nazwisko: %s\n", mojaKlasa.uczen[1].nazwisko);
+    printf("mojaKlasa.uczen[3].imie[2]: %c\n", mojaKlasa.uczen[3].imie[2]);
+
+    return 0;
+}
