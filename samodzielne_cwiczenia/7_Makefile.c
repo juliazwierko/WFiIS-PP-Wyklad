be22fb9d0b9e18d45c688f6d3c3b2d75e5847b38
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-07-04 11:53:49 +0200

    Create 7_Makefile.c

---
+++ samodzielne_cwiczenia/7_Makefile.c
@@ -0,0 +1,14 @@
+//Mamy plik Makefile 
+
+//<...>
+
+//na dysku w katalogu roboczym zapisane mamy pliki:
+//      hw_defs.h hw_hello.h hw_power.h 
+//      hw_hello.c hw_power.c hw_main.c 
+
+//1) Jakie pliki sie pojawia po wykonaniu polecenia make 
+//2) Jaki bedzie efekt wykonania nastepujacej sekwencji komend:
+//         touch hw_defs.h 
+//         make
+
+//Odpowiedz
