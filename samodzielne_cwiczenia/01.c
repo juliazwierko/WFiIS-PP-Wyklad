9a4a51abcdce04a9a949eef562db5e6d7f69fe85
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-07-07 13:45:49 +0200

    Update 01.c

--- samodzielne_cwiczenia/01.c
+++ samodzielne_cwiczenia/01.c
@@ -4,42 +4,42 @@ char tab1[15]
 struct tnode{
   int value;
   float var;
+}
   tab2[10];
   float *TAB = calloc  (10, sizeof(int));
 */
-
 #include <stdio.h>
+#include <stdlib.h>
 
-// Procedura do zapisu tablicy do pliku binarnego
-void zapiszTabliceDoPliku(void* tablica, size_t rozmiar, size_t iloscElementow, FILE* plik) {
-    fwrite(tablica, rozmiar, iloscElementow, plik);
+void zapisz_tablice_do_pliku(void *tablica, size_t rozmiar_elementu, size_t ilosc_elementow, FILE *plik) {
+    fwrite(tablica, rozmiar_elementu, ilosc_elementow, plik);
 }
 
 int main() {
-    char tab1[15];
+    // Przykład użycia dla tablicy 'tab1' typu char
+    char tab1[15] = "Przykladowy tekst";
+    FILE *plik1 = fopen("tablica1.bin", "wb");
+    zapisz_tablice_do_pliku(tab1, sizeof(char), 15, plik1);
+    fclose(plik1);
+
+    // Przykład użycia dla tablicy 'tab2' typu struct tnode
     struct tnode {
         int value;
         float var;
-        int tab2[10];
-        float* TAB;
-    } tn;
+    } tab2[10];
 
-    // Przykładowe inicjalizacje tablicy i struktury tn
-    // ...
+    FILE *plik2 = fopen("tablica2.bin", "wb");
+    zapisz_tablice_do_pliku(tab2, sizeof(struct tnode), 10, plik2);
+    fclose(plik2);
 
-    // Otwarcie pliku do zapisu w trybie binarnym
-    FILE* plik = fopen("plik.bin", "wb");
-    if (plik == NULL) {
-        printf("Nie można otworzyć pliku.\n");
-        return 1;
-    }
+    // Przykład użycia dla dynamicznie alokowanej tablicy 'TAB' typu float *
+    float *TAB = calloc(10, sizeof(float));
 
-    // Wywołanie procedury zapisującej tablicę do pliku
-    zapiszTabliceDoPliku(tab1, sizeof(char), 15, plik);
-    zapiszTabliceDoPliku(&tn, sizeof(struct tnode), 1, plik);
+    FILE *plik3 = fopen("tablica3.bin", "wb");
+    zapisz_tablice_do_pliku(TAB, sizeof(float), 10, plik3);
+    fclose(plik3);
 
-    // Zamknięcie pliku
-    fclose(plik);
+    free(TAB);
 
     return 0;
 }
