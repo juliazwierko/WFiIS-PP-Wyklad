081827e315cb9d6506663843832d1bd2d016c0f9
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-07-07 16:11:32 +0200

    Update 04(dodać_poprawki).c

--- samodzielne_cwiczenia/04(dodać_poprawki).c
+++ samodzielne_cwiczenia/04(dodać_poprawki).c
@@ -14,25 +14,23 @@ struct el {
 
 //b) Funkcja laczaca
 struct el * merge(struct el *L1, struct el *L2){
-    struct el *tmp;
+    if(L1 == NULL && L1 == NULL)
+    {
+        return NULL;
+    }
     if(L2 == NULL)
     {
         return L1;
     }
-    else if(L1 == NULL)
+    if(L1 == NULL)
     {
         return L2;
     }
-    else if(L1 == NULL && L1 == NULL)
+    if(L1->value > L2->value)
     {
-        return NULL;
+        L2->next = merge(L1, L2->next);
+        return L2;
     }
-    //???
-    //else if(L1 == value && L2 == value)
-    // {
-    //     L2->next = merge(L1, L2->next);
-    //     return L2;
-    // }
     else
     {
         L1 -> next = merge(L1->next, L2);
@@ -41,6 +39,5 @@ struct el * merge(struct el *L1, struct el *L2){
 }
 
 int main(int argc, char *argv[]){
-
     return 0;
 }
