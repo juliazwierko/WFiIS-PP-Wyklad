df71474d47ae68a155203ebef6cd71f9e9652b6b
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-07-07 13:50:39 +0200

    Update 01.c

--- samodzielne_cwiczenia/01.c
+++ samodzielne_cwiczenia/01.c
@@ -1,12 +1,8 @@
 /*
 proszę napisz procedurę, która zapisuje dowolna tablice (dowolny typ i rozmiar) do pliku binarnego. Wskaźnik do pliku ma być jednym z argumentów procedury. Proszę zapisać wywołanie dla tablic:
 char tab1[15]
-struct tnode{
-  int value;
-  float var;
-}
-  tab2[10];
-  float *TAB = calloc  (10, sizeof(int));
+struct tnode {int value; float var;}  tab2[10];
+float *TAB = calloc  (10, sizeof(int));
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +13,7 @@ void zapisz_tablice_do_pliku(void *tablica, size_t rozmiar_elementu, size_t ilos
 
 int main() {
     // Przykład użycia dla tablicy 'tab1' typu char
-    char tab1[15] = "tekst";
+    char tab1[15] = "Przykladowy tekst";
     FILE *plik1 = fopen("tablica1.bin", "wb");
     zapisz_tablice_do_pliku(tab1, sizeof(char), 15, plik1);
     fclose(plik1);
